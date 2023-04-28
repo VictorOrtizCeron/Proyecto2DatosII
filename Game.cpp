9d6de490243f42a94b4065e06bd026bed15d49c6
VICTOR ORTIZ <vortiz@estudiantec.cr>
2023-04-28 14:41:55 -0600

    Agregación de Clase Game

---
+++ Game.cpp
@@ -0,0 +1,58 @@
+//
+// Created by vortizc on 4/28/23.
+//
+
+#include "Game.h"
+
+void Game::initWindow(){
+
+    this-> window = new sf::RenderWindow(sf::VideoMode(800, 800), "Ventana de Juego");
+    this-> window ->setFramerateLimit(60);
+    this-> window ->setVerticalSyncEnabled(false);
+}
+
+Game::Game(){
+    this->initWindow();
+}
+Game::~Game()
+{
+    delete this->window;
+
+}
+void Game::updatePollEvents() {
+    sf::Event evento;
+    while(this->window->pollEvent(evento))
+    {
+        if(evento.Event::type==sf::Event::Closed)
+            this->window->close();
+        if(evento.Event::key.code == sf::Keyboard::Escape)
+            this->window->close();
+    }
+}
+ void Game::updateInput(){
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+
+        std::cout<<"w is pressed"<<std::endl;
+    }
+}
+void Game::update(){
+
+    this->updatePollEvents();
+    this->updateInput();
+}
+
+void Game::render(){
+
+    this->window->display();
+}
+void Game::run()
+{
+
+    while(this->window->isOpen())
+    {
+        this->update();
+        this->render();
+
+    }
+
+}
---
+++ Game.h
@@ -0,0 +1,29 @@
+//
+// Created by vortizc on 4/28/23.
+//
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#ifndef PROYECTO2DATOSII_GAME_H
+#define PROYECTO2DATOSII_GAME_H
+
+
+class Game {
+public:
+    Game();
+    virtual ~Game();
+    void initWindow();
+    void run();
+    void update();
+    void render();
+    void updateInput();
+    void updatePollEvents();
+
+
+private:
+    sf::RenderWindow* window;
+
+
+};
+
+
+#endif //PROYECTO2DATOSII_GAME_H
